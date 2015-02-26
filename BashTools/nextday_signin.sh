26a89809774e9407970537129aeda18ed788a28a
machinedao <machinedao@gmail.com>
2015-02-26 15:17:39 +0800

    change server date and open URL
    签到辅助测试工具，把linux服务器的时间改成第二天，然后调用客户端的签到接口进行签到，使用方法{{{./nextday_signin.sh
    token times}}},即第二个参数是token，第三个参数是需要签到的次数

---
+++ BashTools/nextday_signin.sh
@@ -0,0 +1,13 @@
+﻿for ((i=0;i<$2;i++))
+
+do
+        echo $i
+        date -s `date -d'+1 day' +%Y%m%d`
+#       sleep 1s
+
+        date_now=`date +%Y-%m-%d`
+
+        python signin.py $date_now $1
+
+
+done
---
+++ BashTools/signin.py
@@ -0,0 +1,16 @@
+﻿import urllib2,os,sys
+
+# （注意第 0 个参数是脚本名本身）
+
+thedate = sys.argv[1]
+_token = sys.argv[2]
+
+url1 = "http://192.168.xxx.xxx:8090/mobile/api2.do?action=xxxxSignin&signinDate=%s&token=%s" % (thedate,_token)
+url2 = '''&returnType=json&deviceType=Android&xxx'''
+
+url = url1 + url2
+
+data=urllib2.urlopen(url)
+
+print(thedate,"python done")
+
